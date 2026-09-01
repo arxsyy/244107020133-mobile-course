7f7e4fa783ca2174f1c8f2e94c326892639dd480
arxsyy <marsyfernanda@gmail.com>
2026-09-01 17:28:43 +0700

    W1 : Const dan Final

--- Week1/prak1/bin/prak1.dart
+++ Week1/prak1/bin/prak1.dart
@@ -1,3 +1,4 @@
+import 'package:prak1/prak1.dart' as prak1;
 
 void main(List<String> arguments) {
   //print('Hello world: ${prak1.calculate()}!');
---
+++ Week1/prak2/bin/prak2.dart
@@ -0,0 +1,13 @@
+import 'package:prak2/prak2.dart' as prak2;
+const String name = "Marsya";
+const int age = 20;
+
+void main(List<String> arguments) {
+  //print('Hello world: ${prak2.calculate()}!');
+  //name = "Nanda";
+  //const String name = "Nanda";
+  final String nama = "Nanda";
+  final int umur = 21;
+  print ('Nama saya $name, umur saya $age tahun');
+  print ('Nama saya $nama, umur saya $umur tahun');
+}

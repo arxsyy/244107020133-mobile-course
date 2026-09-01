2677db7ef8396818fb76af98d18fb653a6c526b1
arxsyy <marsyfernanda@gmail.com>
2026-09-01 16:47:59 +0700

    W1 : Pengenalan Dart

---
+++ Week1/prak1/bin/prak1.dart
@@ -0,0 +1,14 @@
+
+void main(List<String> arguments) {
+  //print('Hello world: ${prak1.calculate()}!');
+  var nama = 'Marsya';
+  var umur = 20;
+  var tinggi = "160";
+  var alamat = "Jl. Raya No. 1";
+  var iseng = umur + int.parse(tinggi);
+  print("Nama: $nama");
+  print("Umur: $umur");
+  print("Tinggi: $tinggi");
+  print("Alamat: $alamat");
+  print("Hasil Iseng: $iseng");
+}

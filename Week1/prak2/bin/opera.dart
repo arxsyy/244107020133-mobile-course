0619f4767dbb91e509ddbde6c1a7445239514918
arxsyy <marsyfernanda@gmail.com>
2026-09-10 15:39:49 +0700

    W1 : Operator

---
+++ Week1/prak2/bin/opera.dart
@@ -0,0 +1,8 @@
+import 'package:prak2/prak2.dart' as prak2;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Masukkan angka pertama:');
+  String? input1 = stdin.readLineSync();
+  print('Nilainya adalah $input1');
+}
